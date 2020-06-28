cd036335ec1dce75b1b1b6e1c5a80196f76d216e
cw <chenw@shumei.ai>
2020-06-28 17:57:18 +0800

    订单微服务80入驻进eurekaServer

--- cloud-consumer-order80/src/main/java/com/cwcoffee/springcloud/ConsumerOrder80.java
+++ cloud-consumer-order80/src/main/java/com/cwcoffee/springcloud/ConsumerOrder80.java
@@ -2,12 +2,14 @@ package com.cwcoffee.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author cw
  * @Date 2020/06/28 14:52
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerOrder80 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerOrder80.class,args);
