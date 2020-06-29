0f12a25f4eb1432e9f68fdda502918cfcbb6201d
cw <chenw@shumei.ai>
2020-06-29 15:47:23 +0800

    支付微服务集群配置

--- cloud-consumer-order80/src/main/java/com/cwcoffee/springcloud/config/ApplicationConfig.java
+++ cloud-consumer-order80/src/main/java/com/cwcoffee/springcloud/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.cwcoffee.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationConfig {
 
+    @LoadBalanced//开启restTemplate的负载均衡
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
--- cloud-provider-payment8001/src/main/java/com/cwcoffee/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/cwcoffee/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.cwcoffee.springcloud.entity.CommonResult;
 import com.cwcoffee.springcloud.entity.Payment;
 import com.cwcoffee.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,15 +20,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @PostMapping("/insert")
     public CommonResult add(@RequestBody Payment payment){
         int insert = paymentService.insert(payment);
         if(insert > 0){
             log.info("插入成功"+payment);
-            return new CommonResult(200,"插入成功",payment);
+            return new CommonResult(200,"插入成功 "+serverPort,payment);
         }else{
             log.info("插入失败"+insert);
-            return new CommonResult(500,"插入失败",insert);
+            return new CommonResult(500,"插入失败 "+serverPort,insert);
         }
     }
 
@@ -36,10 +40,10 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         if(payment!=null){
             log.info("查询成功"+payment);
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功 "+serverPort,payment);
         }else{
             log.info("查询失败");
-            return new CommonResult(500,"查询失败");
+            return new CommonResult(500,"查询失败 "+serverPort);
         }
     }
 }
