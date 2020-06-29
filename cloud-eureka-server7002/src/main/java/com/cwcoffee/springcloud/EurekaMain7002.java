3508f6508d361202ee6f7ee07ea51ac9d02ae876
cw <chenw@shumei.ai>
2020-06-29 14:36:58 +0800

    eureka集群环境搭建

---
+++ cloud-eureka-server7002/src/main/java/com/cwcoffee/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package com.cwcoffee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author cw
+ * @Date 2020/06/29 14:23
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
