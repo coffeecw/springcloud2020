b3357ec03c55559743bf5609ffb7b37271b10c47
cw <chenw@shumei.ai>
2020-07-01 14:16:19 +0800

    restTemplate.getForObject与restTemplate.getForEntity的区别

--- cloud-consumer-order80/src/main/java/com/cwcoffee/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/cwcoffee/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.cwcoffee.springcloud.controller;
 import com.cwcoffee.springcloud.entity.CommonResult;
 import com.cwcoffee.springcloud.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,18 @@ public class OrderController {
     @GetMapping("/consumer/get/{id}")
     public CommonResult get(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/get/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/getEntity/{id}")
+    public CommonResult getEntity(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/get/" + id, CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            log.info("调用成功");
+            return entity.getBody();
+        }else{
+            log.info("调用失败");
+            return new CommonResult(500,"请求失败");
+        }
     }
 
 }
