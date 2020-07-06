d850b78d2676900689332bad9a84d7b36c4e0ed4
cw <chenw@shumei.ai>
2020-07-06 13:41:47 +0800

    hystrix图形化Dashboard搭建

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/cwcoffee/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.cwcoffee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author cw
+ * @Date 2020/07/06 13:34
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
