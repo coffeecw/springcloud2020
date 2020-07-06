7351983289fd3176f87f9e22ba4e104d83aec1f8
cw <chenw@shumei.ai>
2020-07-06 11:35:07 +0800

    hystrix之服务熔断案例

--- cloud-provider-hystrix-payment8001/src/main/java/com/cwcoffee/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/cwcoffee/springcloud/controller/PaymentController.java
@@ -32,4 +32,11 @@ public class PaymentController {
         log.info("***result"+result);
         return result;
     }
+
+    //====服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        String result = paymentService.paymentCircuitBreaker(id);
+        return result;
+    }
 }
