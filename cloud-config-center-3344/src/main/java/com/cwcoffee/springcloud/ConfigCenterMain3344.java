ca66c975393b3d1365932d1e5785944ec642fec7
cw <chenw@shumei.ai>
2020-07-07 14:17:19 +0800

    config配置总控中心搭建

---
+++ cloud-config-center-3344/src/main/java/com/cwcoffee/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.cwcoffee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author cw
+ * @Date 2020/07/07 11:49
+ */
+@SpringBootApplication
+//开启配置中心服务
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/cwcoffee/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/cwcoffee/springcloud/controller/PaymentController.java
@@ -77,4 +77,9 @@ public class PaymentController {
         return serverPort;
     }
 
+    @GetMapping("/payment/lb")
+    public String paymentLb(){
+        return serverPort;
+    }
+
 }
