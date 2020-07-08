3868fa469a6a3232959a5b6639569ff2caea2a72
cw <chenw@shumei.ai>
2020-07-08 18:00:00 +0800

    Sleuth链路监控

--- cloud-consumer-order80/src/main/java/com/cwcoffee/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/cwcoffee/springcloud/controller/OrderController.java
@@ -19,32 +19,37 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-//    private static final String PAYMENT_URL="http://localhost:8001";
-    private static final String PAYMENT_URL="http://CLOUD-PAYMENT-SERVICE";
+    //    private static final String PAYMENT_URL="http://localhost:8001";
+    private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/insert")
-    public CommonResult insert(Payment payment){
-        return restTemplate.postForObject(PAYMENT_URL+"/insert",payment,CommonResult.class);
+    public CommonResult insert(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/insert", payment, CommonResult.class);
     }
 
     @GetMapping("/consumer/get/{id}")
-    public CommonResult get(@PathVariable("id") Long id){
-        return restTemplate.getForObject(PAYMENT_URL+"/get/"+id,CommonResult.class);
+    public CommonResult get(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/get/" + id, CommonResult.class);
     }
 
     @GetMapping("/consumer/getEntity/{id}")
-    public CommonResult getEntity(@PathVariable("id") Long id){
+    public CommonResult getEntity(@PathVariable("id") Long id) {
         ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/get/" + id, CommonResult.class);
-        if(entity.getStatusCode().is2xxSuccessful()){
+        if (entity.getStatusCode().is2xxSuccessful()) {
             log.info("调用成功");
             return entity.getBody();
-        }else{
+        } else {
             log.info("调用失败");
-            return new CommonResult(500,"请求失败");
+            return new CommonResult(500, "请求失败");
         }
     }
 
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin() {
+        String result = restTemplate.getForObject("http://localhost:8001" + "/payment/zipkin/", String.class);
+        return result;
+    }
 }
