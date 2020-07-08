3a3d5bc68206594305e619581a04702663f4c81d
cw <chenw@shumei.ai>
2020-07-08 17:28:18 +0800

    Stream之group解决消息重复消费

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/cwcoffee/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.cwcoffee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author cw
+ * @Date 2020/07/08 16:37
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
