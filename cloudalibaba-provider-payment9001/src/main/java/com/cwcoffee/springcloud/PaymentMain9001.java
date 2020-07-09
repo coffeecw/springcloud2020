ec0da448931d488d5ffd908601fdbe5a56588126
cw <chenw@shumei.ai>
2020-07-09 18:04:27 +0800

    Nacos之服务提供者注册

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/cwcoffee/springcloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.cwcoffee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author cw
+ * @Date 2020/07/09 17:46
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
